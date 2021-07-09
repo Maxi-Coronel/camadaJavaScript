0474545ccafe3cf53a0d9f63b022e6f6d3c1843a
Maxi-Coronel <maxicoronel1992@gmail.com>
2021-07-09 18:29:46 -0300

    gitignore

--- script/proyecto.js
+++ script/proyecto.js
@@ -1,6 +1,18 @@
+// ===================        Array        ===================
 const productos = [];
 const carrito = [];
 
+// ===================       Const       ===================
+const button = document.getElementById("button");
+const btnCompra = document.getElementById("comprar");
+const cantCarrito = document.getElementById("n-carrito");
+
+// ===================       Eventos       ===================
+button.addEventListener(`click`, agregarProducto);
+button.addEventListener(`click`, agregarCarrito);
+cantCarrito.textContent = `${carrito.length}`;
+
+// ===================  Clase constructora  ===================
 class Producto{
     constructor(titulo, descripcion, precio, stock, estado){
         this.titulo = titulo;
@@ -11,15 +23,7 @@ class Producto{
     }
 }
 
-const button = document.getElementById("button");
-button.addEventListener(`click`, completa);
-
-const btnCompra = document.getElementById("comprar");
-button.addEventListener(`click`, agregarCarrito);
-
-let cantCarrito = document.getElementById("n-carrito");
-cantCarrito.textContent = `${carrito.length}`;
-
+// ===================       Funciones       ===================
 function agregarCarrito() {
 }
 
@@ -28,7 +32,7 @@ function agregarProducto() {
     let titulo = document.getElementById("titulo").value;
     let descripcion = document.getElementById("descripcion").value;
     let precio = document.getElementById("precio").value;
-    let stock = document.getElementById("stock").value;    
+    let stock = document.getElementById("stock").value;
     let estado = document.getElementById("estado").value;
     
     let producto = new Producto(titulo, descripcion, precio, stock, estado);
@@ -41,7 +45,6 @@ function dibujarProducto() {
     let titulo = document.getElementById("titulo").value;
     let descripcion = document.getElementById("descripcion").value;
     let precio = document.getElementById("precio").value;
-    let stock = document.getElementById("stock").value;
         
     let prod = document.getElementById("prod");
 
@@ -77,30 +80,49 @@ function dibujarProducto() {
 
 };
 
-function completa() {
-    agregarProducto();
-    dibujarProducto();
-}
+function dibujarArray() {
 
-console.log(productos);
+    productos.forEach(e => {        
+        let prod = document.getElementById("prod");
+
+        let div1 = document.createElement("div");
+        div1.setAttribute(`class`,"card");    
+        div1.setAttribute(`style`,"width: 18rem;");
+        prod.appendChild(div1);
+
+        let div2 = document.createElement(`div`);
+        div2.setAttribute(`class`,"card-body");
+        div1.appendChild(div2);
+
+        let h5 = document.createElement("h5");
+        h5.textContent = `${e.titulo}`;
+        h5.setAttribute(`class`,"card-title");
+        div2.appendChild(h5);
+
+        let parrafo = document.createElement(`p`);
+        parrafo.textContent = `${e.descripcion}`;
+        parrafo.setAttribute(`class`,"card-text");
+        div2.appendChild(parrafo);
+        
+        let h6 = document.createElement("h6");
+        h6.textContent = `$ ${e.precio}`;
+        h6.setAttribute(`class`,"card-title");
+        div2.appendChild(h6);
+
+        let comprar = document.createElement("input");
+        comprar.setAttribute(`type`,"button");
+        comprar.setAttribute(`class`,"btn-primary btn");
+        comprar.setAttribute(`value`,"comprar");
+        div2.appendChild(comprar);
+    });
+};
+
+function saveLocal(){
+    let aJson = JSON.stringify(Productos);
+    localStorage.setItem("productos",aJson)
+}
 
 /* 
-
-function agregarProductos() {
-    let cantProducto = Number(prompt(`cuantos productos quieres agregar?`));
-    for (let i = 1; i <= cantProducto; i++) {
-        let titulo = prompt(`Dime el titulo del producto`);
-        let descripcion = prompt(`Dime una descripcion de tu producto`);
-        let precio = Number(prompt(`Dime el precio de tu producto`));
-        let stock = Number(prompt(`Dime la cantidad que quieres vender`));
-        console.log(producto);
-        carrito.push(producto);
-    }
-}
-
-function agregarAlCarrito() {
-    carrito.push(producto);
-}
 
 function ordenarElCarrito() {
     let orden = prompt(`Dime cual es el orden que quieres darle al carrito "nombre" o "precio"`);
@@ -125,66 +147,4 @@ function sumarPrecioCarrito() {
     }
 }
 
-agregarProductos();
-ordenarElCarrito();
-
-console.log(carrito);
-sumarPrecioCarrito();
-console.log(sum);
-
-//clase 8 DOM
-
-let foter = document.getElementsByTagName("footer")
-
-console.log(foter);
-
-
-let parrafo = document.createElement("p")
-parrafo.textContent = "ESTE ES EL COPY DESDE TEXT CONTENT"
-foter[0].appendChild(parrafo)
-
-
-
-let div1 = document.getElementById("aca")
-
-
-let div2 = document.createElement("div")
-div1.appendChild(div2)
-
-let h2primero = document.createElement("h2")
-h2primero.textContent = "Este es un subtitulo"
-div1.appendChild(h2primero)
-
-let parrafo = document.createElement("p")
-parrafo.textContent = "ESTE ES EL COPY DESDE TEXT CONTENT"
-parrafo.setAttribute("class", "text-danger container")
-
-div2.appendChild(parrafo)
-
-//foter[0].innerHTML = "<p>ESTE ES EL COPY </p>"
-
-let ocultar = document.querySelector("#id2")
-ocultar.setAttribute("style", "display: none")
-
-
-
-
-
-
-function dibujarProducto() {
-    let input = document.getElementById("titulo").value
-
-    let elemento = document.getElementById("aca")
-    elemento.innerHTML = `Hola tu nombre es ${input}`
-}
-
  */
-
-//boton.addEvListener ?????
-
-
-
-/* function saveLocal(){
-    let aJson = JSON.stringify(Productos);
-    localStorage.setItem("productos",aJson)
-} */
